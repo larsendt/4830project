5ca0de08e2d65cecfc62d43633921509dd7f6efe
Dane Larsen <dane.t.larsen@gmail.com>
2012-04-14 14:24:12 -0600

    fixed chunk alignment, edges should match now

--- src/MapGen.cpp
+++ src/MapGen.cpp
@@ -9,7 +9,7 @@ VoxelCube MapGen::genChunk(int x, int y, int z){
 	CaveNoise cn;
 	HeightNoise hn;
 	
-	int offset[3] = {x*CHUNK_SIZE, y*CHUNK_SIZE, z*CHUNK_SIZE};
+	int offset[3] = {x*CHUNK_SIZE - x, y*CHUNK_SIZE - y, z*CHUNK_SIZE - z};
 	VoxelCube hndata = hn.noise(CHUNK_SIZE, offset, 1, 1.0, 1.0, 0);
 	VoxelCube cndata = cn.noise(CHUNK_SIZE, offset, 1, 1.0, 0.9, 0, 1.0);
 	VoxelCube noisedata = hndata - cndata;
